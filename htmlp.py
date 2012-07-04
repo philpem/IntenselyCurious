d3ee72a43d80b8573178ac02f4da09abaa23904e
Philip Pemberton <philpem@philpem.me.uk>
2012-07-04 11:06:07 +0100

    merge tree-walker into a ParseCommentTree fcn

--- htmlp.py
+++ htmlp.py
@@ -3,30 +3,30 @@ from lxml import etree
 
 CMNT_RE = re.compile(r'IDCommentSubThread(?P<id>[0-9]+)')
 
-def walk(tree, depth=0, parent=0):
-	lp = []
+def ParseCommentTree(innerhtml):
+	def walk(tree, depth=0, parent=0):
+		lp = []
 
-	if 'id' in tree.keys():
-		tid = tree.get('id')
-		x = CMNT_RE.match(tid)
-		if x:
-			cid = int(x.group('id'))
-			lp = lp + [(cid, parent)]
-			#print "  "*depth, depth, parent, int(x.group('id')), tree.tag, " [%s]" % tree.get('id')
-			for i in tree:
-				lp = lp + walk(i, depth+1, int(x.group('id')))
-			return lp
+		if 'id' in tree.keys():
+			tid = tree.get('id')
+			x = CMNT_RE.match(tid)
+			if x:
+				cid = int(x.group('id'))
+				lp = lp + [(cid, parent)]
+				#print "  "*depth, depth, parent, int(x.group('id')), tree.tag, " [%s]" % tree.get('id')
+				for i in tree:
+					lp = lp + walk(i, depth+1, int(x.group('id')))
+				return lp
 
-	for i in tree:
-		lp = lp + walk(i, depth+1, parent)
-	return lp
+		for i in tree:
+			lp = lp + walk(i, depth+1, parent)
+		return lp
+
+	# Fix IntenseDebate's broken XHTML and parse it
+	text = re.sub(r'<p class="idc-fade"<', r'<p class="idc-fade"><', innerhtml)
+	tree = etree.HTML(text)
+	return dict(walk(tree))
 
 f = open('inner_html', 'r')
-
-# Fix IntenseDebate's broken XHTML
-text = re.sub(r'<p class="idc-fade"<', r'<p class="idc-fade"><', f.read())
-
-tree = etree.HTML(text)
-
-print dict(walk(tree))
+print ParseCommentTree(f.read())
 
