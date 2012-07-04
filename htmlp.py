302b3291a991a9cc806a3dbf8112c38e4f72378d
Philip Pemberton <philpem@philpem.me.uk>
2012-07-04 11:00:28 +0100

    fix html parser

--- htmlp.py
+++ htmlp.py
@@ -1,53 +1,38 @@
 import re
-from HTMLParser import HTMLParser
-from htmlentitydefs import name2codepoint
+from lxml import etree
 
-class MyHTMLParser(HTMLParser):
-	RE_THREAD = re.compile(r'ID(CommentSub)?Thread([0-9]+)')
-	RE_COMMENT = re.compile(r'IDComment([0-9]+)')
+CMNT_RE = re.compile(r'IDCommentSubThread(?P<id>[0-9]+)')
 
-	def handle_starttag(self, tag, attrs):
-		if (tag == 'div' or tag == 'span'):
-			for attr in attrs:
-				if attr[0] == 'id':
-					x = self.RE_COMMENT.match(attr[1])
-					if x:
-						print 'Open +', attr[1]
-						print x.group(1)
-						break
-					x = self.RE_THREAD.match(attr[1])
-					if x:
-						print 'Open *', attr[1]
-						print x.group(len(x.groups()))
+comments = {}
 
-"""
-		print "Start tag:", tag
-		for attr in attrs:
-			print "     attr:", attr
-"""
-"""
-	def handle_endtag(self, tag):
-		print "End tag  :", tag
-	def handle_data(self, data):
-		print "Data     :", data
-	def handle_comment(self, data):
-		print "Comment  :", data
-	def handle_entityref(self, name):
-		c = unichr(name2codepoint[name])
-		print "Named ent:", c
-	def handle_charref(self, name):
-		if name.startswith('x'):
-			c = unichr(int(name[1:], 16))
-		else:
-			c = unichr(int(name))
-		print "Num ent  :", ord(c)
-	def handle_decl(self, data):
-		print "Decl     :", data
-"""
+def walk(tree, depth=0, parent=0):
+	walked = False
+
+	if 'id' in tree.keys():
+		tid = tree.get('id')
+		x = CMNT_RE.match(tid)
+		if x:
+			print "  "*depth, depth, parent, int(x.group('id')), tree.tag, " [%s]" % tree.get('id')
+			comments[int(x.group('id'))] = parent
+			#print int(x.group(1))
+			for i in tree:
+				walk(i, depth+1, int(x.group('id')))
+			walked = True
+
+	if not walked:
+		for i in tree:
+			walk(i, depth+1, parent)
+
 
 f = open('inner_html', 'r')
 
-p = MyHTMLParser()
-# IntenseDebate fucked up their HTML. This blob of Magick fixes it.
-p.feed(re.sub(r'<p class="idc-fade"<', r'<p class="idc-fade"><', f.read()))
+# Fix IntenseDebate's broken XHTML
+text = re.sub(r'<p class="idc-fade"<', r'<p class="idc-fade"><', f.read())
+
+tree = etree.HTML(text)
+
+
+walk(tree)
+print comments
+#print etree.tostring(tree, pretty_print=True, method="html")
 
